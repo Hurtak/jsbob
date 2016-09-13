e634819a3c34ae742cea5b0e72bd2bf321ad7558
Petr Huřťák <petr.hurtak@gmail.com>
2016-09-13 21:34:11 +0200

    move stdout functions into log module

---
+++ src/log.js
@@ -0,0 +1,37 @@
+'use strict'
+
+const chalk = require('chalk')
+const lodash = require('lodash')
+
+function indentConsoleOutput (str, indentation = 4) {
+  const indentationStr = lodash.repeat(' ', indentation)
+
+  let out = lodash.split(str, '\n')
+  out = lodash.map(out, line => indentationStr + line)
+  out = lodash.join(out, '\n')
+
+  console.log(out)
+}
+
+function logPrefixed (str, err = false) {
+  let prefix = new Date()
+  prefix = getConsolePrefix(prefix)
+  prefix = chalk.bold.white(prefix)
+
+  str = err ? chalk.bold.red(str) : chalk.bold.green(str)
+
+  console.log(`${prefix} ${str}`)
+}
+
+function getConsolePrefix (date) {
+  const hour = date.getHours()
+  const minutes = lodash.padStart(date.getMinutes(), 2, '0')
+  const seconds = lodash.padStart(date.getSeconds(), 2, '0')
+
+  return `[${hour}:${minutes}:${seconds}]`
+}
+
+module.exports = {
+  prefixed: logPrefixed,
+  indented: indentConsoleOutput
+}
--- src/main.js
+++ src/main.js
@@ -1,64 +1,37 @@
 'use strict'
-
 require('hard-rejection')()
 
 const execa = require('execa')
 const lodash = require('lodash')
-const chalk = require('chalk')
+
+const log = require('./log.js')
 
 // main
 
 async function exec (str) {
-  const prefix = white(getConsolePrefix(new Date()))
-
-  console.log(`${prefix} ${green('running')}`)
-  console.log(`    ${str}`)
+  log.prefixed('running')
+  log.indented(lodash.trim(str))
 
   const {stdout, stderr} = await execa.shell(str)
   if (stdout) {
-    console.log(`${prefix} ${green('stdout')}`)
-    console.log(indentConsoleOutput(stdout, 4))
+    log.prefixed('stdout')
+    log.indented(stdout)
   }
   if (stderr) {
-    console.log(`${prefix} ${red('stderr')}`)
-    console.log(indentConsoleOutput(stderr, 4))
+    log.prefixed('stderr', true)
+    log.indented(stderr)
   }
 }
 
 async function main () {
-  // const res = await exec('ls --help')
-  // const res = await exec('svgo --help')
-  // const res = await exec('cat test/app/images/controls/next.svg | svgo -i - -o -')
-  const res = await exec('svgo -f test/app/images/controls -o test/dist')
-  // const res = await exec('dir')
-  // await execa('mkdir', ['test'])
-  // await execa('rm -r', ['test'])
-  // const res2 = await execa('ll')
-  // console.log(res2)
+//   await exec('ls --help')
+//   await exec('svgo --help')
+//   await exec('cat test/app/images/controls/next.svg | svgo -i - -o -')
+//   await exec('svgo -f test/app/images/controls -o test/dist')
+  await exec(`
+    echo "multiline first"
+    echo "multiline second"
+  `)
 }
 
 main()
-
-// utils
-
-function getConsolePrefix (date) {
-  const hour = date.getHours()
-  const minutes = lodash.padStart(date.getMinutes(), 2, '0')
-  const seconds = lodash.padStart(date.getSeconds(), 2, '0')
-
-  return `[${hour}:${minutes}:${seconds}]`
-}
-
-function white (str) { return chalk.bold.white(str) }
-function green (str) { return chalk.bold.green(str) }
-function red (str) { return chalk.bold.red(str) }
-
-function indentConsoleOutput (str, indentation = 4) {
-  const indentationStr = lodash.repeat(' ', indentation)
-
-  let out = lodash.split(str, '\n')
-  out = lodash.map(out, (line => indentationStr + line))
-  out = lodash.join(out, '\n')
-
-  return out
-}
